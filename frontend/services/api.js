3b7f78bec1f04e2641bc4b9a8e11e3524d39dd7f
workinghard90 <autumnmhall90@gmail.com>
2025-05-20 07:58:51 -0400

    Delete frontend directory

--- frontend/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = process.env.EXPO_PUBLIC_API_URL || 'https://<https://aetherionai-mobile.onrender.com>';
-
-export const getUniverse = async () => {
-  const res = await axios.get(`${API_URL}/api/items`);
-  return res.data;
-};
