3f929feead017e9f8945b5f64d6303577a06389f
workinghard90 <autumnmhall90@gmail.com>
2025-05-20 17:19:49 -0400

    Update app.config.js

--- apps/aetherion-mobile/app.config.js
+++ apps/aetherion-mobile/app.config.js
@@ -1,3 +1,9 @@
+// apps/aetherion-mobile/App.tsx
+import { registerRootComponent } from 'expo';
+import EmanationViewer from './screens/EmanationViewer'; // or your main screen
+
+registerRootComponent(EmanationViewer);
+
 import 'dotenv/config';
 
 export default {
