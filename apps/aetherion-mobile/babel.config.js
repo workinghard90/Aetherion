03d4ae1159aecd27008b341a176904f00f5f441b
workinghard90 <autumnmhall90@gmail.com>
2025-05-22 19:03:51 -0400

    Update babel.config.js

--- apps/aetherion-mobile/babel.config.js
+++ apps/aetherion-mobile/babel.config.js
@@ -1,8 +1,7 @@
-// apps/aetherion-mobile/babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: ['react-native-reanimated/plugin']
   };
 };
