0f12c4af06d968bd910158c83b3f0117fbc4823c
workinghard90 <autumnmhall90@gmail.com>
2025-05-22 21:32:17 -0400

    Update app.config.js

--- apps/aetherion-mobile/app.config.js
+++ apps/aetherion-mobile/app.config.js
@@ -1,18 +1,33 @@
-// apps/aetherion-mobile/app.config.js
-import 'dotenv/config';
-
-export default {
-  expo: {
+export default () => {
+  return {
     name: "AetherionAI",
-    slug: "aetherionai",
+    slug: "aetherion-mobile",
     version: "1.0.0",
     orientation: "portrait",
-    platforms: ["ios", "android", "web"],
+    icon: "./assets/icon.png",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    updates: {
+      fallbackToCacheTimeout: 0
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      }
+    },
     web: {
-      bundler: "metro"
+      favicon: "./assets/favicon.png"
     },
     extra: {
-      apiUrl: process.env.EXPO_PUBLIC_API_URL
+      apiUrl: process.env.EXPO_PUBLIC_API_URL || "https://localhost:5000"
     }
-  }
+  };
 };
