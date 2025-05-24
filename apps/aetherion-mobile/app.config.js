cc070cb5f8f062e9c82fbf7a80c794d630d020dd
workinghard90 <autumnmhall90@gmail.com>
2025-05-23 21:26:53 -0400

    Update app.config.js

--- apps/aetherion-mobile/app.config.js
+++ apps/aetherion-mobile/app.config.js
@@ -1,33 +1,39 @@
 // apps/aetherion-mobile/app.config.js
 
-export default () => ({
-  name: "AetherionAI",
-  slug: "aetherion-mobile",
-  version: "1.0.0",
-  orientation: "portrait",
-  icon: "./assets/icons/icon.png",
+import 'dotenv/config';
+
+export default ({ config }) => ({
+  name: 'AetherionAI',
+  slug: 'aetherion-mobile',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icons/icon.png',
   splash: {
-    image: "./assets/splash/splash.png",
-    resizeMode: "contain",
-    backgroundColor: "#ffffff"
+    image: './assets/splash/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff'
   },
   updates: {
     fallbackToCacheTimeout: 0
   },
-  assetBundlePatterns: ["**/*"],
+  assetBundlePatterns: ['**/*'],
   ios: {
     supportsTablet: true
   },
   android: {
     adaptiveIcon: {
-      foregroundImage: "./assets/icons/adaptive-icon.png",
-      backgroundColor: "#ffffff"
+      foregroundImage: './assets/icons/adaptive-icon.png',
+      backgroundColor: '#ffffff'
     }
   },
   web: {
-    favicon: "./assets/meta/favicon.png"
+    favicon: './assets/meta/favicon.png'
   },
   extra: {
-    apiUrl: process.env.EXPO_PUBLIC_API_URL || "http://localhost:5000"
-  }
+    apiUrl: process.env.EXPO_PUBLIC_API_URL || 'http://localhost:5000',
+    env: process.env.NODE_ENV || 'development'
+  },
+  plugins: [
+    // add additional Expo plugins here if needed
+  ]
 });
