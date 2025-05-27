d118125ce70fe899c93b7a8c25ccdb63a12dd1d5
workinghard90 <autumnmhall90@gmail.com>
2025-05-27 02:27:46 -0400

    Update veil_of_the_grove.py

--- veil_of_the_grove.py
+++ veil_of_the_grove.py
@@ -1,5 +1,3 @@
-# services/backend/veil_of_the_grove.py
-
 from app import create_app
 from flask_cors import CORS
 import os
@@ -8,5 +6,5 @@ app = create_app()
 CORS(app)
 
 if __name__ == "__main__":
-    debug_mode = os.getenv("FLASK_ENV", "production") == "development"
-    app.run(debug=debug_mode, host="0.0.0.0", port=5000)
+    is_debug = os.getenv("FLASK_ENV", "production") == "development"
+    app.run(debug=is_debug, host="0.0.0.0", port=5000)
