e947e50f529374124994c7d1a799c584f8c0bdab
workinghard90 <autumnmhall90@gmail.com>
2025-05-27 04:31:44 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_migrate import Migrate
 from .database import db
 from .api.auth import auth_bp
 from .api.vault import vault_bp
+from .api.health import health_bp  # optional ping route
 
 migrate = Migrate()
 
@@ -13,10 +14,8 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
 
-    with app.app_context():
-        db.create_all()
-
     app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(vault_bp, url_prefix="/vault")
+    app.register_blueprint(health_bp)  # optional /ping
 
     return app
