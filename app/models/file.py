1a369ea82a2f4ccbb7841803a4d255a23b73c3f3
workinghard90 <autumnmhall90@gmail.com>
2025-05-27 05:48:46 -0400

    Update file.py

--- app/models/file.py
+++ app/models/file.py
@@ -1,16 +1,9 @@
-# services/backend/app/models/file.py
-
-from datetime import datetime
-from ..database import db
+from aetherion.extensions import db
 
 class VaultFile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(255), nullable=False)
-    original_name = db.Column(db.String(255), nullable=False)
-    upload_time = db.Column(db.DateTime, default=datetime.utcnow)
-    mime_type = db.Column(db.String(100))
+    filename = db.Column(db.String(120), nullable=False)
+    original_name = db.Column(db.String(120), nullable=False)
+    mime_type = db.Column(db.String(120))
     size = db.Column(db.Integer)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-
-    title = db.Column(db.String(255), nullable=True)
-    tags = db.Column(db.String(255), nullable=True)
+    user_id = db.Column(db.Integer, nullable=False)
