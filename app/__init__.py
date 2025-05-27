cefb9b8f5df32c6ba134e51033a1d63b84d2f9da
workinghard90 <autumnmhall90@gmail.com>
2025-05-27 02:04:15 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,22 @@
-# This file marks the tests/ directory as a package.
-# Required for pytest discovery and relative imports.
+from flask import Flask
+from flask_migrate import Migrate
+from .database import db
+from .api.auth import auth_bp
+from .api.vault import vault_bp
+
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("config.DevelopmentConfig")
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    with app.app_context():
+        db.create_all()
+
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+    app.register_blueprint(vault_bp, url_prefix="/vault")
+
+    return app
