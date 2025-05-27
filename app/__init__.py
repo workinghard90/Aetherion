913978131973f4a825e74485c014554ea2fe5aa7
workinghard90 <autumnmhall90@gmail.com>
2025-05-27 05:58:59 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-```python
 from .auth import auth_bp
 from .vault import vault_bp
 from .health import health_bp
@@ -7,4 +6,3 @@ def register_blueprints(app):
     app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(vault_bp, url_prefix="/vault")
     app.register_blueprint(health_bp)
-```
