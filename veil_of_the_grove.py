b61d379524ac64a445b957b858a8f28b56640396
workinghard90 <autumnmhall90@gmail.com>
2025-05-27 02:49:29 -0400

    Update veil_of_the_grove.py

--- veil_of_the_grove.py
+++ veil_of_the_grove.py
@@ -7,4 +7,5 @@ CORS(app)
 
 if __name__ == "__main__":
     is_debug = os.getenv("FLASK_ENV", "production") == "development"
-    app.run(debug=is_debug, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # required for Render
+    app.run(debug=is_debug, host="0.0.0.0", port=port)
