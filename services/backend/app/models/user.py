490e1f80f541758985f1b97ae0e1c42da5ac489c
workinghard90 <autumnmhall90@gmail.com>
2025-05-27 01:03:48 -0400

    Update user.py

--- services/backend/app/models/user.py
+++ services/backend/app/models/user.py
@@ -1,15 +1,8 @@
 # services/backend/app/models/user.py
 
 from ..database import db
-from passlib.hash import bcrypt
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(255), nullable=False)
-
-    def set_password(self, password):
-        self.password_hash = bcrypt.hash(password)
-
-    def check_password(self, password):
-        return bcrypt.verify(password, self.password_hash)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
