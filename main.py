55cc441a632b7111c6e37a5809d7854a0cdb3a02
workinghard90 <autumnmhall90@gmail.com>
2025-05-27 22:15:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+# === main.py ===
+
 from aetherion import create_app
 
 app = create_app()
-
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    debug = os.getenv("FLASK_ENV") == "development"
-    app.run(debug=debug, host="0.0.0.0", port=port)
