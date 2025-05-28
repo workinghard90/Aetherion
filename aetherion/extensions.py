decbeae3d8ecac5336eaab8f96d42ee7a3dfaaa6
workinghard90 <autumnmhall90@gmail.com>
2025-05-28 14:59:58 -0500

    Update and rename extensions.py to aetherion/extensions.py

--- aetherion/extensions.py
+++ aetherion/extensions.py
@@ -1,7 +1,7 @@
+# aetherion/extensions.py
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
-cors = CORS()
