7972e4fac58c48a0f710ea6ecb64970647aeabd2
workinghard90 <autumnmhall90@gmail.com>
2025-05-28 17:08:00 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: ".", // assumes vite.config.js is in frontend/
   plugins: [react()],
-  server: {
-    port: 3000
-  }
+  build: {
+    outDir: "dist",
+  },
 });
