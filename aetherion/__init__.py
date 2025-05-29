511eb892e572f7eb036826f171066e6b0448d4ba
workinghard90 <autumnmhall90@gmail.com>
2025-05-28 22:42:34 -0500

    Update __init__.py

--- aetherion/__init__.py
+++ aetherion/__init__.py
@@ -1,8 +1,9 @@
+# aetherion/__init__.py
+
 from flask import Flask
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from aetherion.extensions import db, migrate, cors
-from aetherion.models import User
 from aetherion.routes import register_routes
 from aetherion.admin.views import SecureAdmin
 import aetherion.models as models
@@ -11,9 +12,11 @@ import inspect
 def create_app():
     app = Flask(__name__)
     app.config.from_prefixed_env()
+
     db.init_app(app)
     migrate.init_app(app, db)
     cors.init_app(app)
+
     register_routes(app)
 
     admin = Admin(app, index_view=SecureAdmin(), template_mode="bootstrap4")
