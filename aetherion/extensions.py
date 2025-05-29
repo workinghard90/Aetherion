62d7dc2101c8bd896725023bc8d2c8224d2941f3
workinghard90 <autumnmhall90@gmail.com>
2025-05-28 20:04:15 -0500

    Update extensions.py

--- aetherion/extensions.py
+++ aetherion/extensions.py
@@ -1,7 +1,7 @@
 # aetherion/extensions.py
-
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 db = SQLAlchemy()
-migrate = Migrate()
+admin = Admin(name="Aetherion Admin", template_mode="bootstrap4")
