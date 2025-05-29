cb0611957ca7a1a1ca10312238ca19d4890bce0a
workinghard90 <autumnmhall90@gmail.com>
2025-05-29 13:18:04 -0500

    Update button.py

--- aetherion/models/button.py
+++ aetherion/models/button.py
@@ -1,9 +1,8 @@
-from datetime import datetime
 from aetherion.extensions import db
 
 class Button(db.Model):
+    __tablename__ = "buttons"
     id = db.Column(db.String, primary_key=True)
     label = db.Column(db.String, nullable=False)
     sigil = db.Column(db.String, nullable=False)
     url = db.Column(db.String, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
