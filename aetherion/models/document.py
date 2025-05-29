ed415bef7d1b819892189aed5c98569d667cd574
workinghard90 <autumnmhall90@gmail.com>
2025-05-28 22:34:19 -0500

    Update document.py

--- aetherion/models/document.py
+++ aetherion/models/document.py
@@ -1,8 +1,11 @@
 from aetherion.extensions import db
 
 class Document(db.Model):
+    __tablename__ = "documents"
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text, nullable=True)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def __repr__(self):
+        return f"<Document {self.id} - {self.title}>"
