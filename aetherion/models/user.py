b771a5c3459a40f2ff7b78904352d4fec35e5f0a
workinghard90 <autumnmhall90@gmail.com>
2025-05-28 20:11:24 -0500

    Update user.py

--- aetherion/models/user.py
+++ aetherion/models/user.py
@@ -3,4 +3,5 @@ from aetherion.extensions import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
