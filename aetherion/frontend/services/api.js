df3dd36fad704771c4258680d6e2f4ba04cdb108
workinghard90 <autumnmhall90@gmail.com>
2025-05-30 16:49:47 -0700

    Update api.js

--- aetherion/frontend/services/api.js
+++ aetherion/frontend/services/api.js
@@ -2,6 +2,4 @@ import axios from 'axios';
 const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export const getUniverse    = () => axios.get(`${API_URL}/api/items`).then(r => r.data);
-export const getUniverseItem= id => axios.get(`${API_URL}/api/items/${id}`).then(r => r.data);
-export const createEntity   = data => axios.post(`${API_URL}/api/items`, data).then(r => r.data);
 export const getScrolls     = () => axios.get(`${API_URL}/api/scrolls`).then(r => r.data);
