99c85bfd3c1364be89fba5f79713d207e5498cec
workinghard90 <autumnmhall90@gmail.com>
2025-05-30 16:46:25 -0700

    Update App.js

--- aetherion/frontend/App.js
+++ aetherion/frontend/App.js
@@ -1,34 +1,16 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import LoginScreen    from './screens/LoginScreen';
-import HomeScreen     from './screens/HomeScreen';
-import VaultScreen    from './screens/VaultScreen';
-import ScrollsScreen  from './screens/ScrollsScreen';
-import OracleScreen   from './screens/OracleScreen';
-import OthersGate     from './screens/OthersGate';
-
-const Stack = createStackNavigator();
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Vault" component={VaultScreen} />
-          <Stack.Screen name="Scrolls" component={ScrollsScreen} />
-          <Stack.Screen name="Oracle" component={OracleScreen} />
-          <Stack.Screen name="OthersGate" component={OthersGate} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
-  );
+{
+  "expo": {
+    "name": "Aetherion Mobile",
+    "slug": "aetherion-mobile",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/icon.png",
+    "splash": {
+      "image": "./assets/splash.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#6200ee"
+    },
+    "assetBundlePatterns": ["**/*"],
+    "web": { "favicon": "./assets/favicon.png" }
+  }
 }
-
-const styles = StyleSheet.create({
-  container: { flex: 1 }
-});
