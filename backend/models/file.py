3f8e5ba933d56d03b655a5821f3088f5174848a0
workinghard90 <autumnmhall90@gmail.com>
2025-05-31 16:28:31 -0400

    Update file.py

--- backend/models/file.py
+++ backend/models/file.py
@@ -4,9 +4,9 @@ class VaultFile(db.Model):
     __tablename__ = "vault_files"
 
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(255), nullable=False)        # encrypted filename on disk
-    original_name = db.Column(db.String(255), nullable=False)   # user‐uploaded original name
+    filename = db.Column(db.String(255), nullable=False)        # encrypted file name on disk
+    original_name = db.Column(db.String(255), nullable=False)   # user uploaded original name
     mime_type = db.Column(db.String(100), nullable=False)
-    size = db.Column(db.Integer, nullable=False)                # size in bytes (encrypted)
+    size = db.Column(db.Integer, nullable=False)                # size of encrypted data
     user_id = db.Column(db.Integer, nullable=False)             # uploader’s user ID
     uploaded_at = db.Column(db.DateTime, server_default=db.func.now())
