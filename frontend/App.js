422bc0e920a4d2babc479d5833750408f629344a
workinghard90 <autumnmhall90@gmail.com>
2025-05-31 16:38:51 -0400

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,32 +1,78 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import LoginScreen    from './screens/LoginScreen';
-import HomeScreen     from './screens/HomeScreen';
-import VaultScreen    from './screens/VaultScreen';
-import ScrollsScreen  from './screens/ScrollsScreen';
-import OracleScreen   from './screens/OracleScreen';
-import OthersGate     from './screens/OthersGate';
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import VaultScreen from "./screens/VaultScreen";
+import ScrollsScreen from "./screens/ScrollsScreen";
+import OracleScreen from "./screens/OracleScreen";
+import OthersGate from "./screens/OthersGate";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Login"         component={LoginScreen} />
-          <Stack.Screen name="Home"          component={HomeScreen} />
-          <Stack.Screen name="Vault"         component={VaultScreen} />
-          <Stack.Screen name="Scrolls"       component={ScrollsScreen} />
-          <Stack.Screen name="Oracle"        component={OracleScreen} />
-          <Stack.Screen name="OthersGate"    component={OthersGate} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        {/* 1) Login */}
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{
+            headerShown: false,
+            title: "Login"
+          }}
+        />
+        {/* 2) OthersGate (post-login) */}
+        <Stack.Screen
+          name="OthersGate"
+          component={OthersGate}
+          options={{
+            headerShown: false
+          }}
+        />
+        {/* 3) Vault / Archive */}
+        <Stack.Screen
+          name="Vault"
+          component={VaultScreen}
+          options={{
+            title: "Vault of Secure Files",
+            headerStyle: { backgroundColor: "#6200ee" },
+            headerTintColor: "#fff"
+          }}
+        />
+        {/* 4) Scrolls / Archive Viewer */}
+        <Stack.Screen
+          name="Scrolls"
+          component={ScrollsScreen}
+          options={{
+            title: "Library of Scrolls",
+            headerStyle: { backgroundColor: "#6200ee" },
+            headerTintColor: "#fff"
+          }}
+        />
+        {/* 5) Oracle Chat */}
+        <Stack.Screen
+          name="Oracle"
+          component={OracleScreen}
+          options={{
+            title: "Grove Oracle",
+            headerStyle: { backgroundColor: "#6200ee" },
+            headerTintColor: "#fff"
+          }}
+        />
+        {/* 6) Home (Dashboard) */}
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "Sanctum of Aetherion",
+            headerStyle: { backgroundColor: "#6200ee" },
+            headerTintColor: "#fff"
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({ container: { flex:1 } });
