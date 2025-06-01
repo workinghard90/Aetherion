c558c2ab9b547eb2dbda512b463b9dd5c4a1abe5
workinghard90 <autumnmhall90@gmail.com>
2025-06-01 09:45:41 -0700

    Update and rename backend/config.py to backend/aetherion/config.py

---
+++ backend/aetherion/config.py
@@ -0,0 +1,5 @@
+# Aetherion/backend/aetherion/config.py
+
+# This file is optional if you want to break out config into Python.
+# Currently we just use config.json + environment variables.  You can remove this file 
+# or keep it if you want to define extra defaults here.
