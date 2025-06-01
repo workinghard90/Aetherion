d6e6b2df5ab47f67202991fde7e82b7efc245f6e
workinghard90 <autumnmhall90@gmail.com>
2025-05-31 23:53:41 -0700

    Update and rename app.config.js to expo.config.js

---
+++ frontend/expo.config.js
@@ -0,0 +1,41 @@
+// expo.config.js
+import 'dotenv/config'; // only needed if you want to load local .env when running expo locally
+
+export default ({ config }) => {
+  return {
+    ...config,
+    expo: {
+      ...config.expo,
+      name: "Aetherion",                          // display name
+      slug: "aetherion-mobile",
+      version: "1.0.0",
+      orientation: "portrait",
+      icon: "./assets/icon.png",
+      userInterfaceStyle: "light",
+      splash: {
+        image: "./assets/splash.png",
+        resizeMode: "contain",
+        backgroundColor: "#6200ee",
+      },
+      assetBundlePatterns: ["**/*"],
+      ios: {
+        supportsTablet: true,
+      },
+      android: {
+        adaptiveIcon: {
+          foregroundImage: "./assets/icon.png",
+          backgroundColor: "#6200ee",
+        },
+      },
+      web: {
+        favicon: "./assets/favicon.png",
+      },
+      extra: {
+        // Any environment variable references must go here in JS.
+        apiUrl: process.env.EXPO_PUBLIC_API_URL
+          ? process.env.EXPO_PUBLIC_API_URL + "/api"
+          : "https://aetherion-mobile.onrender.com/api",
+      },
+    },
+  };
+};
