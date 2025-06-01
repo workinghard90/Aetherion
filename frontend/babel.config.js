80f438cf66ea3500dd60f3c9aca4232489536551
workinghard90 <autumnmhall90@gmail.com>
2025-05-31 23:55:05 -0700

    Update babel.config.js

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,7 +1,10 @@
+// frontend/babel.config.js
 module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
+    plugins: [
+      'react-native-reanimated/plugin'   // <-- required if you installed reanimated
+    ],
   };
 };
