05a2059b8d08cb6951619bc65d327ed677003080
workinghard90 <autumnmhall90@gmail.com>
2025-06-01 21:36:39 -0700

    Update and rename app.config.js to app.config.cjs

---
+++ frontend/app.config.cjs
@@ -0,0 +1,35 @@
+require('dotenv/config');
+
+module.exports = () => ({
+  expo: {
+    name: 'Aetherion',
+    slug: 'aetherion-mobile',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#6200ee',
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/icon.png',
+        backgroundColor: '#6200ee',
+      },
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+    extra: {
+      apiUrl:
+        process.env.EXPO_PUBLIC_API_URL ||
+        'https://aetherion-mobile.onrender.com/api',
+    },
+  },
+});
