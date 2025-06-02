e6c1c2a23a3c8382927d740df713872fd5069c3c
workinghard90 <autumnmhall90@gmail.com>
2025-06-01 20:42:45 -0700

    Update babel.config.js

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,15 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
-
-  const presets = ["babel-preset-expo"];
-  const plugins = [];
-
-  // Only use Reanimated’s Babel plugin when _not_ building for web:
-  // (Expo sets BABEL_ENV or NODE_ENV to "web" during `expo export:web`.)
-  const isWeb = api.caller((caller) => caller && caller.platform === "web");
-  if (!isWeb) {
-    plugins.push("react-native-reanimated/plugin");
-  }
-
-  return { presets, plugins };
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      // Re-animated’s plugin must come last
+      "react-native-reanimated/plugin"
+    ],
+  };
 };
