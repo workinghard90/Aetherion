1b440c363cb6766acf3ffd92429a2f909dcbbc2c
workinghard90 <autumnmhall90@gmail.com>
2025-06-01 17:51:09 -0700

    Update api.js

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,33 +1,28 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import axios from 'axios';
+// Aetherion/frontend/services/api.js
 
-const API_BASE = process.env.EXPO_PUBLIC_API_URL || "https://aetherion-mobile.onrender.com/api";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Constants from "expo-constants";
 
-export const storeToken = async (token) => {
-  try {
-    await AsyncStorage.setItem("authToken", token);
-  } catch {
-    // ignore
-  }
-};
+const API_BASE = Constants.expoConfig.extra.apiUrl || "https://aetherion-mobile.onrender.com/api";
 
 export const getToken = async () => {
-  try {
-    return await AsyncStorage.getItem("authToken");
-  } catch {
-    return null;
-  }
+  return await AsyncStorage.getItem("token");
 };
 
-export const apiClient = axios.create({
+export const axiosInstance = axios.create({
   baseURL: API_BASE,
-  headers: { "Content-Type": "application/json" }
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
-apiClient.interceptors.request.use(async (config) => {
+// Request interceptor to attach JWT if present
+axiosInstance.interceptors.request.use(async (config) => {
   const token = await getToken();
   if (token) {
-    config.headers["Authorization"] = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
-}, (error) => Promise.reject(error));
+});
