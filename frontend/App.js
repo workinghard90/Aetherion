cf4e4895770c708edaf3773649300611b7e1c348
workinghard90 <autumnmhall90@gmail.com>
2025-06-01 17:41:36 -0700

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,48 +1,63 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './screens/LoginScreen';
-import HomeScreen from './screens/HomeScreen';
-import VaultScreen from './screens/VaultScreen';
-import ScrollsScreen from './screens/ScrollsScreen';
-import OracleScreen from './screens/OracleScreen';
-import OthersGate from './screens/OthersGate';
+// Aetherion/frontend/App.js
+
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import LoginScreen from "./screens/LoginScreen";
+import DashboardScreen from "./screens/DashboardScreen";
+import VaultScreen from "./screens/VaultScreen";
+import ScrollsScreen from "./screens/ScrollsScreen";
+import OracleScreen from "./screens/OracleScreen";
+import OthersGate from "./screens/OthersGate";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen 
-          name="Login" 
-          component={LoginScreen} 
-          options={{ headerShown: false }} 
+      <Stack.Navigator
+        initialRouteName="OthersGate"
+        screenOptions={{
+          headerStyle: { backgroundColor: "#6200ee" },
+          headerTintColor: "#fff",
+          headerTitleStyle: { fontWeight: "bold" }
+        }}
+      >
+        <Stack.Screen
+          name="OthersGate"
+          component={OthersGate}
+          options={{ title: "The Gate of Others" }}
         />
-        <Stack.Screen 
-          name="Home" 
-          component={HomeScreen} 
-          options={{ headerShown: false }} 
+
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: "Login to Aetherion" }}
         />
-        <Stack.Screen 
-          name="Vault" 
-          component={VaultScreen} 
-          options={{ title: "🗝️ The Vault" }} 
+
+        <Stack.Screen
+          name="Dashboard"
+          component={DashboardScreen}
+          options={{ title: "Sovereign’s Realm" }}
         />
-        <Stack.Screen 
-          name="Scrolls" 
-          component={ScrollsScreen} 
-          options={{ title: "📜 Archive of Scrolls" }} 
+
+        <Stack.Screen
+          name="Vault"
+          component={VaultScreen}
+          options={{ title: "🔐 The Vault" }}
         />
-        <Stack.Screen 
-          name="Oracle" 
-          component={OracleScreen} 
-          options={{ title: "🌳 The Grove" }} 
+
+        <Stack.Screen
+          name="Scrolls"
+          component={ScrollsScreen}
+          options={{ title: "📜 The Archive of Scrolls" }}
         />
-        <Stack.Screen 
-          name="OthersGate" 
-          component={OthersGate} 
-          options={{ headerShown: false }} 
+
+        <Stack.Screen
+          name="Oracle"
+          component={OracleScreen}
+          options={{ title: "🔮 The Grove" }}
         />
       </Stack.Navigator>
     </NavigationContainer>
