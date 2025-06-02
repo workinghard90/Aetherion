a4c26bbfaea813b4f232e6b67d1e83f56a742e52
workinghard90 <autumnmhall90@gmail.com>
2025-06-01 17:47:43 -0700

    Delete backend/scrolls directory

--- backend/scrolls/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint
-scrolls_bp = Blueprint("scrolls", __name__, url_prefix="/api/scrolls")
-
-from .routes import *  # import route functions
--- backend/scrolls/routes.py
+++
@@ -1,7 +0,0 @@
-from . import scrolls_bp
-from flask import jsonify
-
-@scrolls_bp.route("/", methods=["GET"])
-def list_scrolls():
-    # Return a JSON list of “scroll” metadata (e.g. from a DB table)
-    return jsonify([]), 200
