77ce69cb33025cfa953c5cbcf20afbbc59b2d3b2
workinghard90 <autumnmhall90@gmail.com>
2025-06-02 22:07:11 -0400

    Update and rename backend/aetherion/models.py to backend/models.py

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,5 @@
-# Aetherion/backend/aetherion/models.py
-
-from aetherion.extensions import db
-from werkzeug.security import generate_password_hash
+from backend.extensions import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,6 +8,9 @@ class User(db.Model):
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class File(db.Model):
     id = db.Column(db.Integer, primary_key=True)
