204db9781e71e66004d6b679f48cd296ca472876
workinghard90 <autumnmhall90@gmail.com>
2025-06-02 21:50:18 -0400

    Update extensions.py

--- backend/aetherion/extensions.py
+++ backend/aetherion/extensions.py
@@ -1,3 +1,12 @@
+# Aetherion/backend/aetherion/extensions.py
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+
+db = SQLAlchemy()
+jwt = JWTManager()
+cors = CORS()
 # backend/aetherion/extensions.py
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
