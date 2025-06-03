8b2cb57b15f7c41ff9963b9ed144b0b0a97d762e
workinghard90 <autumnmhall90@gmail.com>
2025-06-02 20:22:09 -0700

    Update extensions.py

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,9 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
-from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
 db = SQLAlchemy()
-jwt = JWTManager()
-cors = CORS()
 migrate = Migrate()
+cors = CORS()
+jwt = JWTManager()
