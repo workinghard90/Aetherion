69381cc357569dba1c8f79aa669a10e110db6027
workinghard90 <autumnmhall90@gmail.com>
2025-06-02 20:25:11 -0700

    Update scrolls.py

--- backend/scrolls.py
+++ backend/scrolls.py
@@ -1,19 +1,9 @@
 from flask import Blueprint, jsonify
-from backend.models import File
+from .models import Scroll
 
-scrolls_bp = Blueprint("scrolls", __name__)
+bp = Blueprint("scrolls", __name__)
 
-@scrolls_bp.route("/", methods=["GET"])
-def list_scrolls():
-    files = File.query.all()
-    return jsonify([{
-        "id": f.id,
-        "original_name": f.original_name,
-        "uploaded_at": f.uploaded_at.isoformat(),
-        "user_id": f.user_id
-    } for f in files])
-
-@scrolls_bp.route("/<int:file_id>", methods=["GET"])
-def fetch_scroll(file_id):
-    file = File.query.get_or_404(file_id)
-    return file.blob  # Can be wrapped in send_file if desired
+@bp.route("/", methods=["GET"])
+def get_all_scrolls():
+    scrolls = Scroll.query.all()
+    return jsonify([{"id": s.id, "title": s.title} for s in scrolls])
