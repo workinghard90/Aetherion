0d1a96a01a0b3c22d133652e73fe120df0a367a4
workinghard90 <autumnmhall90@gmail.com>
2025-06-02 20:35:31 -0700

    Update babel.config.js

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,10 +1,18 @@
 module.exports = function(api) {
-  api.cache(false);
+  api.cache(true);
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      // If you’re using React Native Reanimated v3 on web, leave this:
-      "react-native-reanimated/plugin"
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@components": "./components",
+            "@screens": "./screens",
+            "@services": "./services"
+          }
+        }
+      ]
     ]
   };
 };
