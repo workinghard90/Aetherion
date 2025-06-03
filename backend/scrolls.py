d0323494946a492173a30675e5b6290218bdeac0
workinghard90 <autumnmhall90@gmail.com>
2025-06-02 22:10:15 -0400

    Update and rename backend/aetherion/scrolls.py to backend/scrolls.py

--- backend/scrolls.py
+++ backend/scrolls.py
@@ -1,7 +1,5 @@
-# Aetherion/backend/aetherion/scrolls.py
-
-from flask import Blueprint, jsonify, request
-from aetherion.models import File
+from flask import Blueprint, jsonify
+from backend.models import File
 
 scrolls_bp = Blueprint("scrolls", __name__)
 
@@ -18,4 +16,4 @@ def list_scrolls():
 @scrolls_bp.route("/<int:file_id>", methods=["GET"])
 def fetch_scroll(file_id):
     file = File.query.get_or_404(file_id)
-    return file.blob  # or optionally send_file/BytesIO as download
+    return file.blob  # Can be wrapped in send_file if desired
