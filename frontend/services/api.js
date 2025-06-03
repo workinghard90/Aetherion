18d71b4c3cc770027e03ceb561515262bde010d2
workinghard90 <autumnmhall90@gmail.com>
2025-06-02 22:14:15 -0400

    Update api.js

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,28 +1,16 @@
-// Aetherion/frontend/services/api.js
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import axios from "axios";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import Constants from "expo-constants";
-
-const API_BASE = Constants.expoConfig.extra.apiUrl || "https://aetherion-mobile.onrender.com/api";
-
-export const getToken = async () => {
-  return await AsyncStorage.getItem("token");
-};
-
-export const axiosInstance = axios.create({
-  baseURL: API_BASE,
-  timeout: 10000,
-  headers: {
-    "Content-Type": "application/json"
-  }
+const api = axios.create({
+  baseURL: 'https://your-backend-url.com/api'
 });
 
-// Request interceptor to attach JWT if present
-axiosInstance.interceptors.request.use(async (config) => {
-  const token = await getToken();
+api.interceptors.request.use(async (config) => {
+  const token = await AsyncStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
+
+export default api;
