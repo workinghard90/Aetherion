75b5bdafa97527bd2a973688f6ea28ad295b1816
workinghard90 <autumnmhall90@gmail.com>
2025-06-02 22:06:52 -0400

    Update and rename backend/aetherion/extensions.py to backend/extensions.py

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,15 +1,9 @@
-# Aetherion/backend/aetherion/extensions.py
-
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 jwt = JWTManager()
 cors = CORS()
-# backend/aetherion/extensions.py
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-db = SQLAlchemy()
 migrate = Migrate()
